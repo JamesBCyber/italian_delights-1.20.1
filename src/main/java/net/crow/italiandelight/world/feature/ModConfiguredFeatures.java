20aaf68450652c6672a627150e9de5dc3108ad29
pugfi <Calderon.mikeyjr@gmail.com>
2025-12-17 16:43:39 -0500

    Fix Mod ID issues, Implemented Olive Trees

--- src/main/java/net/crow/italiandelight/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/crow/italiandelight/world/feature/ModConfiguredFeatures.java
@@ -1,60 +1,32 @@
 package net.crow.italiandelight.world.feature;
 
-import net.crow.italiandelight.ItalianDelightMain;
 import net.crow.italiandelight.init.BlockInit;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
-import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+import net.crow.italiandelight.ItalianDelightMain;
+import java.util.List;
 
 public class ModConfiguredFeatures {
-
-    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
-            DeferredRegister.create(Registries.CONFIGURED_FEATURE, ItalianDelightMain.MOD_ID);
-
     public static final ResourceKey<ConfiguredFeature<?, ?>> OLIVE_KEY = registerKey("olive");
 
-//    public static final RegistryObject<ConfiguredFeature<?, ?>> OLIVE_TREE =
-//            CONFIGURED_FEATURES.register("olive_tree",() ->
-//                    new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
-//                            BlockStateProvider.simple(BlockInit.OLIVE_LOG.get()),
-//                            new StraightTrunkPlacer(5, 6, 3),
-//                            BlockStateProvider.simple(BlockInit.OLIVE_LEAVES.get()),
-//                            new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
-//                            new TwoLayersFeatureSize(1, 0, 2)).build()));
-
-//    public static final RegistryObject<ConfiguredFeature<?, ?>> OLIVE_TREE_SPAWN =
-//            CONFIGURED_FEATURES.register("olive_tree_spawn", () -> new ConfiguredFeature<>(Feature.RANDOM_SELECTOR,
-//                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(
-//                            ModPlacedFeatures.OLIVE_TREE_CHECKED.getHolder().get(),
-//                            0.5f)), ModPlacedFeatures.OLIVE_TREE_CHECKED.getHolder().get())));
-
-//    public static final RegistryObject<ConfiguredFeature<?, ?>> WILD_GRAPES =
-//            CONFIGURED_FEATURES.register("wild_grapes", () -> new ConfiguredFeature<>(Feature.FLOWER,
-//                    new RandomPatchConfiguration(64,10,2, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK,
-//                            new SimpleBlockConfiguration(BlockStateProvider.simple(BlockInit.WILD_GRAPES.get()))))));
-//
-//    public static final RegistryObject<ConfiguredFeature<?, ?>> WILD_HERBS =
-//            CONFIGURED_FEATURES.register("wild_herbs", () -> new ConfiguredFeature<>(Feature.FLOWER,
-//                    new RandomPatchConfiguration(32,6,2, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK,
-//                            new SimpleBlockConfiguration(BlockStateProvider.simple(BlockInit.WILD_HERBS.get()))))));
-
-    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context){
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
         register(context, OLIVE_KEY, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                 BlockStateProvider.simple(BlockInit.OLIVE_LOG.get()),
                 new StraightTrunkPlacer(5, 4, 3),
@@ -74,7 +46,4 @@ public class ModConfiguredFeatures {
                                                                                           ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
         context.register(key, new ConfiguredFeature<>(feature, configuration));
     }
-
-
-    public static void register(IEventBus eventBus) {CONFIGURED_FEATURES.register(eventBus);}
 }
--- src/main/java/net/crow/italiandelight/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/crow/italiandelight/world/feature/ModPlacedFeatures.java
@@ -1,39 +1,40 @@
 package net.crow.italiandelight.world.feature;
 
-import net.crow.italiandelight.ItalianDelightMain;
+import net.crow.italiandelight.init.BlockInit;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.world.level.levelgen.placement.BiomeFilter;
-import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.minecraft.world.level.levelgen.placement.RarityFilter;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
-
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.crow.italiandelight.ItalianDelightMain;
 import java.util.List;
 
-
 public class ModPlacedFeatures {
-    public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
-            DeferredRegister.create(Registries.PLACED_FEATURE, ItalianDelightMain.MOD_ID);
+    public static final ResourceKey<PlacedFeature> OLIVE_PLACED_KEY = registerKey("olive_placed");
+
+    public static void bootstrap(BootstapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        register(context, OLIVE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OLIVE_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        BlockInit.OLIVE_SAPLING.get()));
+    }
 
 
-//    public static final RegistryObject<PlacedFeature> GRAPES_PLACED = PLACED_FEATURES.register("patch_wild_grapes",
-//            () -> new PlacedFeature(ModConfiguredFeatures.WILD_GRAPES.getHolder().get(),
-//                    List.of(RarityFilter.onAverageOnceEvery((Integer) /*Configuration.CHANCE_WILD_GRAPES.get()*/ 100), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome())));
-//
-//    public static final RegistryObject<PlacedFeature> HERBS_PLACED = PLACED_FEATURES.register("patch_wild_herbs",
-//            () -> new PlacedFeature(ModConfiguredFeatures.WILD_HERBS.getHolder().get(),
-//                    List.of(RarityFilter.onAverageOnceEvery((Integer) /*Configuration.CHANCE_WILD_HERBS.get()*/ 100), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome())));
+    private static ResourceKey<PlacedFeature> registerKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(ItalianDelightMain.MOD_ID, name));
+    }
 
-//    public static final RegistryObject<PlacedFeature> OLIVE_TREE_PLACED = PLACED_FEATURES.register("olive_tree_placed",
-//            () -> new PlacedFeature(ModConfiguredFeatures.OLIVE_TREE_SPAWN.getHolder().get(),
-//                    VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.05f, 1), BlockInit.OLIVE_SAPLING.get())));//,PlacementUtils.HEIGHTMAP, BiomeFilter
-//    // .biome())));
-
-    //    public static final RegistryObject<PlacedFeature> OLIVE_TREE_CHECKED = PLACED_FEATURES.register("olive_tree_checked",
-//            () -> new PlacedFeature(ModConfiguredFeatures.OLIVE_KEY.getHolder().get(),
-//                    List.of(PlacementUtils.filteredByBlockSurvival(BlockInit.OLIVE_SAPLING.get()))));
-    public static void register(IEventBus eventBus) {PLACED_FEATURES.register(eventBus);}
+    private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
+                                 List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
 }
